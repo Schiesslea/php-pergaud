d8d61a7a13961c3079c299677aff9f58e17baae6
Andy Schiessle <andyschiessle@gmail.com>
2024-01-31 17:52:39 +0100

    bootstrap

---
+++ projets/php-web-intro/icones/index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gluten:wght@100..900&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: "Gluten", cursive;
+        }
+    </style>
+    <title>Icones</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Les icones</h1>
+        <ul class="my-4">
+            <li>BootStrap</li>
+            <li>Google</li>
+            <li>Font awesome</li>
+        </ul>
+
+        <section>
+            <h2><i class="bi bi-arrow-right-circle fs-2 text-primary me-2"></i>BootStrap</h2>
+            <div class="d-flex">
+            <i class="bi bi-arrow-right-circle fs-2 text-primary me-2 "></i>
+            <p class="h2 ">Titre</p>
+            </div>
+
+            <i class="bi bi-arrow-right-circle fs-2 text-primary me-2 "></i>
+            <p class="h2 d-inline">Titre</p>
+            <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" color="#669988" fill="currentColor" class="bi bi-badge-wc-fill" viewBox="0 0 16 16">
+                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm11.666 1.89c.682 0 1.139.47 1.187 1.107H14v-.11c-.053-1.187-1.024-2-2.342-2-1.604 0-2.518 1.05-2.518 2.751v.747c0 1.7.905 2.73 2.518 2.73 1.314 0 2.285-.792 2.342-1.939v-.114h-1.147c-.048.615-.497 1.05-1.187 1.05-.839 0-1.318-.62-1.318-1.727v-.742c0-1.112.488-1.754 1.318-1.754zm-6.188.926h.044L6.542 11h1.006L9 5.001H7.818l-.82 4.355h-.056L5.97 5.001h-.94l-.972 4.355h-.053l-.827-4.355H2L3.452 11h1.005z"/>
+            </svg>
+            </div>
+        </section>
+        <section>
+            <h2>Google</h2>
+            <span class="material-symbols-outlined text-primary fs-1">search</span>
+            <span class="material-symbols-outlined fs-1">smart_toy</span>
+        </section>
+        <section>
+            <h2>Font awesome</h2>
+            <i class="fa-solid fa-house fs-1 text-warning "></i>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM164.1 325.5C182 346.2 212.6 368 256 368s74-21.8 91.9-42.5c5.8-6.7 15.9-7.4 22.6-1.6s7.4 15.9 1.6 22.6C349.8 372.1 311.1 400 256 400s-93.8-27.9-116.1-53.5c-5.8-6.7-5.1-16.8 1.6-22.6s16.8-5.1 22.6 1.6zM144.4 208a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm192-32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
+        </section>
+        <section>
+            <h2>Fonts google</h2>
+            <p>Exemple</p>
+            <p style="font-weight: 200">Exemple</p>
+            <p style="font-weight: 300">Exemple</p>
+            <p style="font-weight: 400">Exemple</p>
+            <p style="font-weight: 500">Exemple</p>
+            <p style="font-weight: 600">Exemple</p>
+            <p style="font-weight: 700">Exemple</p>
+            <p style="font-weight: 800">Exemple</p>
+            <p style="font-weight: 900">Exemple</p>
+        </section>
+    </div>
+</body>
+</html>
